f6d6f366ea1fa009a80a9579899f3eb708db729e
josephkem <josephkemoy@gmail.com>
2020-09-09 02:29:19 -0400

    Modified the get, update and delete methods to return the same error response if the bootcamp isn't found

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -61,7 +61,9 @@ exports.updateBootcamp = async (req, res, next) => {
     });
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
 
     res.status(200).json({ success: true, data: bootcamp });
@@ -78,7 +80,9 @@ exports.deleteBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false });
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
     }
 
     res.status(200).json({ success: true, data: {} });
