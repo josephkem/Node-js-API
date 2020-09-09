689053f8c712daa63e077f60ecb6faeb3d678766
josephkem <josephkemoy@gmail.com>
2020-09-09 02:23:21 -0400

    Replaced the errors in the https methods from the controller so that all the errors ae handled in the error file

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,7 +10,7 @@ exports.getBootcamps = async (req, res, next) => {
 
     res.status(200).json({ success: true, data: bootcamps });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -30,9 +30,7 @@ exports.getBootcamp = async (req, res, next) => {
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
     //res.status(400).json({ success: false });
-    next(
-      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-    );
+    next(err);
   }
 };
 
@@ -48,7 +46,7 @@ exports.createBootcamp = async (req, res, next) => {
       data: bootcamp,
     });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -68,7 +66,7 @@ exports.updateBootcamp = async (req, res, next) => {
 
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    return res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -85,6 +83,6 @@ exports.deleteBootcamp = async (req, res, next) => {
 
     res.status(200).json({ success: true, data: {} });
   } catch (err) {
-    return res.status(400).json({ success: false });
+    next(err);
   }
 };
