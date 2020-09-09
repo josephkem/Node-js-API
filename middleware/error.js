b4cfc9a688375145b03e543de5658c9aa2f372be
josephkem <josephkemoy@gmail.com>
2020-09-09 02:46:27 -0400

    Added an error handler to handle duplicate fields being entered

--- middleware/error.js
+++ middleware/error.js
@@ -4,14 +4,21 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
 
   error.message = err.message;
-  console.log(err.stack.red);
+
+  //Log to console for dev
+  console.log(err);
 
   //Mongoose bad ObjectId
   if (err.name === "CastError") {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
   }
-  console.log(error.name);
+
+  //Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = "Duplicate field entered";
+    error = new ErrorResponse(message, 400);
+  }
 
   res.status(error.statusCode || 500).json({
     success: false,
