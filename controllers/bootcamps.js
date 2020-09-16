d25b74156d3274be7c0557470f8f89190896110b
josephkem <josephkemoy@gmail.com>
2020-09-15 22:04:09 -0400

    N/A late

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,8 @@
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
+const geocoder = require('../utils/geocoder');
 const Bootcamp = require("../models/Bootcamp");
+
 
 //@desc  GET all bootcamps
 //@route GET /api/v1/bootcamps
@@ -69,4 +71,33 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   }
 
   res.status(200).json({ success: true, data: {} });
+});
+
+//@desc  Get bootcamps within a radius
+//@route GET /api/v1/bootcamps/radius/:zipcode/:distance
+//@access Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  //Get lat/long from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const long = loc[0].longitude;
+
+  //Calculate radius using radians
+  //Divide distance by radius of Earth
+  //Earth Radius = 3,963 mi / 6,378 km
+  const radius = distance / 3963 ;
+
+  const bootcamps = await Bootcamp.find({
+    location: {$geoWithin: { $centerSphere: [ [ long, lat], radius ]} }
+
+  })
+
+  res.status(200).json({
+    succes: true,
+    count: bootcamp.length,
+    data: bootcamps
+  })
+  
 });
